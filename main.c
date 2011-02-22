107dd9c4bca115aeb04b448fc017f271692630b1
Rob Taylor <rob.taylor@codethink.co.uk>
2011-02-22 20:03:58 +0000

    Make server return something on GET

--- main.c
+++ main.c
@@ -1,5 +1,6 @@
 #include <glib.h>
 #include <libsoup/soup.h>
+#include <string.h>
 
 
 
@@ -11,21 +12,28 @@ void server_callback (SoupServer *server,
                       gpointer user_data)
 {
 	const char *response = "{}";
-/*    if (msg->method != SOUP_METHOD_GET) {
-        soup_message_set_status (msg, SOUP_STATUS_NOT_IMPLEMENTED);
-        return;
-    }
-*/
-    soup_message_set_status (msg, SOUP_STATUS_OK);
-    soup_message_set_response (msg, "application/json", SOUP_MEMORY_STATIC,
-                   response, strlen(response));
+	if (msg->method != SOUP_METHOD_GET) {
+		soup_message_set_status (msg, SOUP_STATUS_NOT_IMPLEMENTED);
+		return;
+	}
+
+	g_debug("got server callback");
+	soup_message_set_status (msg, SOUP_STATUS_OK);
+	soup_message_set_response (msg, "application/json", SOUP_MEMORY_STATIC,
+				   response, strlen(response));
 
 }
 
 int main()
 {
+	g_type_init();
+
 	SoupServer *server = soup_server_new (
-		SOUP_SERVER_PORT, 3000);
-	soup_server_add_handler (server, "/", server_callback,
+		SOUP_SERVER_PORT, 3000,
+		NULL);
+	soup_server_add_handler (server, NULL, server_callback,
              NULL,NULL);
+
+	GMainLoop* main_loop = g_main_loop_new (NULL, FALSE);
+	soup_server_run(server);
 }
